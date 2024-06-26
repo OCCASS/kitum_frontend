2029b5d0b8b5d11456eac61e0133450e1112fa4d
OCCCAS <pavlov.timur556@yandex.ru>
2024-06-26 11:40:58 +0300

    fix: fixed when SubmitButton changed size when showing spinner

--- components/ui/SubmitButton.tsx
+++ components/ui/SubmitButton.tsx
@@ -3,9 +3,18 @@
 import { useFormStatus } from "react-dom";
 import Button from "./Button";
 import Spinner from "./Spinner";
+import { twMerge } from "tailwind-merge";
 
 export default function SubmitButton({ children, className }: { children: React.ReactNode, className?: string }) {
     const { pending } = useFormStatus()
 
-    return <Button className={className} type="submit">{pending ? <Spinner className="m-auto" /> : children}</Button>
+    return <Button className={twMerge("relative", className)} type="submit">
+        <div className={pending ? "invisible" : "visible"}>{children}</div>
+        <Spinner className={
+            twMerge(
+                "absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2",
+                pending ? "block" : "hidden"
+            )
+        } />
+    </Button>
 }
