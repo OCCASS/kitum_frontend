819fd22706ab46bc8135abea4cd3efd7ec65345c
OCCCAS <pavlov.timur556@yandex.ru>
2024-06-28 14:24:49 +0300

    feat: user profile refactoring

--- app/(core)/me/actions.ts
+++ app/(core)/me/actions.ts
@@ -1,4 +1,4 @@
-import { post } from "@/lib/fetch";
+import { post, postFormData } from "@/lib/fetch";
 import IUser from "@/types/user";
 
 export async function editUser(prevState: any, formData: FormData) {
@@ -7,5 +7,13 @@ export async function editUser(prevState: any, formData: FormData) {
         lastName: formData.get("lastName")
     })
     if (status === 200) return { message: "", user: data }
+    return { message: "Edit failed.", user: null }
+}
+
+export async function editUserAvatar(prevState: any, formData: FormData) {
+    const { data, status } = await postFormData<IUser>(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/me/edit_avatar/`, formData
+    )
+    if (status === 201) return { message: "", user: data }
     return { message: "Edit failed.", user: null }
 }
--- app/(core)/me/page.tsx
+++ app/(core)/me/page.tsx
@@ -2,63 +2,91 @@
 
 import Input from "@/components/ui/Input"
 import Image from "next/image"
-import { ChangeEvent, useEffect, useRef } from "react"
+import { ChangeEvent, useEffect, useRef, useState } from "react"
 import { useFormState } from "react-dom"
-import { editUser } from "./actions"
+import { editUser, editUserAvatar } from "./actions"
 import { useUser } from "@/lib/providers/user"
 import SubmitButton from "@/components/ui/SubmitButton"
-import Button from "@/components/ui/Button"
 import { CameraIcon } from "@heroicons/react/24/outline"
-import { postFormData } from "@/lib/fetch"
-import IUser from "@/types/user"
+import Button from "@/components/ui/Button"
 
 export default function Me() {
     const { user, setUser } = useUser()
-    const [state, action] = useFormState(editUser, { message: "", user: null })
-    const fileInputRef = useRef<HTMLInputElement>(null)
-
-    useEffect(() => {
-        if (state.user) setUser(state.user)
-    }, [state, setUser])
-
-    const onAvatarButtonClick = () => {
-        fileInputRef?.current?.click()
-    }
-
-    const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
-        if (!e.target.files) return
-        const file = e.target.files[0]
-        const formData = new FormData()
-        formData.append("avatar", file)
-        const { data, status } = await postFormData<IUser>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user/me/edit_avatar/`, formData)
-        if (status === 201) setUser(data)
-    }
 
     if (!user) return null
 
     return (
-        <div className="space-y-5">
-            <section className="space-y-8">
-                <h1>Привет, {user.firstName} {user.lastName}</h1>
-                <div className="relative inline-block">
-                    <Image src={user.avatar} alt="Аватарка" width="100" height="100" className="rounded-full object-cover size-24" />
-                    <Button variant="none" className="bg-gray-200 p-3 rounded-full absolute right-0 bottom-0 transform translate-x-1/3 translate-y-1/3" onClick={onAvatarButtonClick}>
-                        <CameraIcon className="size-5" strokeWidth="2" />
-                        <Input type="file" className="hidden" innerRef={fileInputRef} onChange={onFileChange} accept="image/*" />
-                    </Button>
-                </div>
-                <form action={action} className="flex gap-3 flex-col md:flex-row">
-                    <div className="flex gap-3">
-                        <Input placeholder="Имя" defaultValue={user?.firstName} className="flex-1" name="firstName" />
-                        <Input placeholder="Фамилия" defaultValue={user?.lastName} className="flex-1" name="lastName" />
-                    </div>
-                    <SubmitButton>Изменить</SubmitButton>
-                </form>
+        <div className="space-y-8">
+            <h1>Привет, {user.firstName} {user.lastName}</h1>
+            <section className="flex items-center gap-10">
+            </section>
+            <EditUserAvatarForm />
+            <section>
+                <EditUserForm />
             </section>
             <section id="subscribtion">
                 <h1 className="mb-5">Текущая подписка</h1>
                 <p>Бла-бла-бла пока этой информации нет...</p>
             </section>
         </div>
+    )
+}
+
+function EditUserForm() {
+    const { user, setUser } = useUser()
+    const [state, action] = useFormState(editUser, { message: "", user: null })
+
+    useEffect(() => {
+        if (state.user) setUser(state.user)
+    }, [state, setUser])
+
+    return (
+        <form action={action} className="flex gap-3 flex-col md:flex-row">
+            <div className="flex gap-3">
+                <Input placeholder="Имя" defaultValue={user?.firstName} className="flex-1" name="firstName" />
+                <Input placeholder="Фамилия" defaultValue={user?.lastName} className="flex-1" name="lastName" />
+            </div>
+            <SubmitButton>Изменить</SubmitButton>
+        </form>
+    )
+}
+
+
+function EditUserAvatarForm() {
+    const { user, setUser } = useUser()
+    const [editUserAvatarState, editUserAvatarAction] = useFormState(editUserAvatar, { message: "", user: null })
+    const [file, setFile] = useState<File | null>(null)
+    const fileInputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        if (editUserAvatarState.user) {
+            setUser(editUserAvatarState.user)
+            setFile(null)
+        }
+    }, [editUserAvatarState, setUser])
+
+    const openFileInput = () => {
+        fileInputRef?.current?.click()
+    }
+
+    const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files) return
+        const file = e.target.files[0]
+        setFile(file)
+    }
+
+    return (
+        <>
+            <div className="relative w-fit">
+                <Image src={user?.avatar ?? ""} alt="Аватарка" width="100" height="100" className="rounded-full object-cover size-24" />
+                <Button variant="none" className="bg-gray-200 p-3 rounded-full absolute right-0 bottom-0 transform translate-x-1/3 translate-y-1/3" onClick={openFileInput}>
+                    <CameraIcon className="size-5" strokeWidth="2" />
+                </Button>
+            </div>
+            <form action={editUserAvatarAction}>
+                <Input type="file" name="avatar" className="hidden" accept="image/*" innerRef={fileInputRef} onChange={onFileChange} />
+                {file && <SubmitButton variant="outline" className="text-sm">Изменить</SubmitButton>}
+            </form>
+        </>
     )
 }
