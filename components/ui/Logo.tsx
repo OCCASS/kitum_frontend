e3f722d17eed7971af0ae7d55fea2a39c73e0361
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-02 11:52:33 +0300

    feat: added logo file

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,10 +1,16 @@
+import logoIcon from "@/public/logo.svg"
+import Image from "next/image"
+
 type TLogoProps = {
     className?: string
 }
 
 const Logo = ({ className }: TLogoProps) => {
     return (
-        <p className={`text-xl font-bold ${className}`}>KITUM</p>
+        <p className={`text-xl font-bold ${className} flex items-center gap-1`}>
+            <Image priority src={logoIcon} alt="🐳" className="size-8" />
+            KITUM
+        </p>
     )
 }
 
