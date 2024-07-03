65ec04aa34df68c7beaa8a7f1d159f1e0455daca
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-03 15:07:29 +0300

    feat: added calendar to schedule page

--- app/(core)/schedule/page.tsx
+++ app/(core)/schedule/page.tsx
@@ -1,8 +1,10 @@
+import Calendar from "@/components/Calendar";
+
 export default function Schedule() {
     return (
         <div className="space-y-3">
             <h1>Расписание</h1>
+            <Calendar />
         </div>
     )
 }
-
