fab6ad04bbfcdf4f0ae08c5c7ca66ce42494567a
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-12 22:48:42 +0300

    feat: inner shadow added

--- components/TasksView/TasksPaginator/Mobile.tsx
+++ components/TasksView/TasksPaginator/Mobile.tsx
@@ -1,16 +1,31 @@
+import { useRef, useState } from "react"
 import { TTasksPaginatorProps } from "."
 import TasksBarItem from "./Item"
 
 export default function MobilePaginator({ tasks, selected, setSelected }: TTasksPaginatorProps) {
+    const scrollableRef = useRef(null);
+    const [isScrolledToLeft, setIsScrolledToLeft] = useState(true);
+    const [isScrolledToRight, setIsScrolledToRight] = useState(false);
+
+    const onScroll = () => {
+        if (!scrollableRef.current) return
+        const scrollableElement = scrollableRef.current;
+        const { scrollLeft, scrollWidth, clientWidth } = scrollableElement;
+        setIsScrolledToLeft(scrollLeft === 0);
+        setIsScrolledToRight(scrollLeft + clientWidth === scrollWidth);
+    }
+
     return (
-        <ul className="flex md:hidden gap-2 py-2 overflow-y-auto inner-right-shadow">
-            {tasks.map((task, index) => <TasksBarItem
-                key={index}
-                index={index}
-                isCorrect={task.isCorrect}
-                isSelected={selected === index}
-                onClick={setSelected}
-            />)}
-        </ul>
+        <div className={`inner-x-shadow ${!isScrolledToLeft ? "show-left" : ""} ${isScrolledToRight ? "hide-right" : ""}`}>
+            <ul className="flex md:hidden gap-2 py-2 overflow-y-auto" onScroll={onScroll} ref={scrollableRef}>
+                {tasks.map((task, index) => <TasksBarItem
+                    key={index}
+                    index={index}
+                    isCorrect={task.isCorrect}
+                    isSelected={selected === index}
+                    onClick={setSelected}
+                />)}
+            </ul>
+        </div>
     )
 }
