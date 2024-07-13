cd6cf0e1ec9007e0a7909817aa5c696920e4d43f
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-13 23:08:03 +0300

    feat: added chevron animation in UserProfile

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,6 +1,6 @@
 import IUser from "@/types/user";
 import { ReactNode, useCallback, useRef, useState } from "react";
-import { ArrowRightEndOnRectangleIcon, MoonIcon, DocumentIcon, CheckCircleIcon, BellAlertIcon, SunIcon, ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
+import { ArrowRightEndOnRectangleIcon, MoonIcon, DocumentIcon, CheckCircleIcon, BellAlertIcon, SunIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import { signout } from "@/app/actions";
 import useOutsideClick from "@/lib/hooks/useOutsideClick";
 import Link from "next/link";
@@ -15,9 +15,9 @@ export default function UserProfile({ user }: { user: IUser }) {
 
     return (
         <div className="relative">
-            <div className="flex gap-2 items-center cursor-pointer" onClick={() => setShowDropdown(prev => !prev)}>
+            <div className="flex gap-2 items-center cursor-pointer" onClick={() => setShowDropdown(true)}>
                 <UserProfileImage className="size-12" user={user} key="profile-image" />
-                {showDropdown ? <ChevronUpIcon className="size-4" /> : <ChevronDownIcon className="size-4" />}
+                <ChevronDownIcon className={`size-4 transition-transform ${showDropdown ? "rotate-180" : ""}`} />
                 <UserProfileDropdown user={user} show={showDropdown} close={closeDropdown} />
             </div>
         </div>
