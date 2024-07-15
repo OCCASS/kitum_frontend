a9687ce1367faeacdf327652f7271fcce69381f6
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-15 11:56:44 +0300

    fix: user profile image refactor

--- components/ui/UserProfileImage.tsx
+++ components/ui/UserProfileImage.tsx
@@ -2,12 +2,12 @@ import IUser from "@/types/user";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 
-export default function UserProfileImage({ user, src, className }: { user: IUser, src?: string, className?: string }) {
+export default function UserProfileImage({ user, src, size }: { user: IUser, src?: string, size?: number }) {
     return <Image
         src={src ? src : user.avatar}
         alt={user.firstName.charAt(0) + user.lastName.charAt(0)}
         width="128"
         height="128"
-        className={twMerge("rounded-full object-cover", className)}
+        className={twMerge("rounded-full object-cover", `size-${size}`)}
     />
 }
