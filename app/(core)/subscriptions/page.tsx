fb336c2b75c6cd24c07e8684760e6b7c0cebaca6
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-20 00:51:10 +0300

    feat: added subscriptions plug

--- app/(core)/subscriptions/page.tsx
+++ app/(core)/subscriptions/page.tsx
@@ -6,10 +6,16 @@ import { get } from "@/lib/fetch";
 
 export default async function Page() {
     const { data } = await get<ISubscription[]>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/subscriptions/unpurchased/`)
+
     return (
         <div className="space-y-3">
             <h1>Подписки</h1>
-            <div className="feed">{data.map(item => <Subscription key={item.id} subscription={item} />)}</div>
+            {
+                data.length === 0 ?
+                    <p className="text-center text-gray-500">Все подписки куплены!</p>
+                    :
+                    <div className="feed">{data.map(item => <Subscription key={item.id} subscription={item} />)}</div>
+            }
         </div>
     )
 }
