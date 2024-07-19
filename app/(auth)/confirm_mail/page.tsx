5a67e5f944984e467f8d7d24b3cdb555671c6034
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-20 00:46:28 +0300

    fix: confirm mail page fixed, always show token is invalid message

--- app/(auth)/confirm_mail/page.tsx
+++ app/(auth)/confirm_mail/page.tsx
@@ -1,23 +1,36 @@
-"use server"
+"use client"
 
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
-export default async function ConfirmMail({ searchParams }: { searchParams: { t: string } }) {
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_AUTH_BASE_URL}/confirm_mail/`,
-        {
-            method: "POST",
-            body: JSON.stringify({ token: searchParams.t }),
-            headers: {
-                "Content-Type": "application/json"
-            },
-            cache: "no-cache"
+export default function ConfirmMail({ searchParams }: { searchParams: { t: string } }) {
+    const [status, setStatus] = useState<number>()
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_AUTH_BASE_URL}/confirm_mail/`,
+                {
+                    method: "POST",
+                    body: JSON.stringify({ token: searchParams.t }),
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    cache: "no-cache"
+                }
+            )
+            setIsLoading(false)
+            setStatus(response.status)
         }
-    )
+        fetchData()
+    }, [])
 
-    if (response.status === 200) return <>
+    if (isLoading) return <p>Loading...</p>
+
+    if (status === 200) return <>
         <p>Адрес почты подтвержден!</p>
-        <p>Для входа в аккаунт можете перейти на страницу <Link href="/signin">входа</Link></p>
+        <p>Для входа в аккаунт можете перейти на страницу <Link href="/signin" className="text-blue">входа</Link></p>
     </>
 
     return <p>Токен не валидный или просрочен!</p>
