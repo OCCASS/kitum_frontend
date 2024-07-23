2c7eb8c1319f410718ca58dcd28f684cfa2679f2
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-23 17:24:29 +0300

    feat: added confetti if user completed and reached 90 and heighr score

--- components/LessonTasksView.tsx
+++ components/LessonTasksView.tsx
@@ -8,33 +8,55 @@ import Button from "@/components/ui/Button";
 import Link from "next/link";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { TTaskAnswer } from "@/types/task";
+import dynamic from "next/dynamic";
+
+
+const Fireworks = dynamic(() => import("react-canvas-confetti/dist/presets/fireworks"))
 
 
 export default function LessonTasksView({ data }: { data: ILesson }) {
     const [lesson, setLesson] = useState<ILesson>(data)
+    const [showConfetti, setShowConfetti] = useState(false)
 
     const complete = async () => {
-        const { data, status } = await post<ILesson>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/lessons/${lesson?.id}/complete_tasks/`)
-        if (status === 200) setLesson(data)
+        const {
+            data,
+            status
+        } = await post<ILesson>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/lessons/${lesson?.id}/complete_tasks/`)
+        if (status === 200) {
+            setLesson(data)
+            setShowConfetti(data.result !== null && data.result >= 90)
+        }
     }
 
     const skip = async (taskId: string) => {
-        const { data, status } = await post<ILesson>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/lessons/${lesson?.id}/${taskId}/skip/`)
+        const {
+            data,
+            status
+        } = await post<ILesson>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/lessons/${lesson?.id}/${taskId}/skip/`)
         if (status === 200) setLesson(data)
     }
 
     const answer = async (taskId: string, answer: TTaskAnswer) => {
-        const { data, status } = await post<ILesson>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/lessons/${lesson?.id}/${taskId}/answer/`, { answer })
+        const {
+            data,
+            status
+        } = await post<ILesson>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/lessons/${lesson?.id}/${taskId}/answer/`, { answer })
         if (status === 200) setLesson(data)
     }
 
     return (
         <div className="space-y-2 max-w-prose m-auto">
-            <Link href={`/lessons/${lesson.id}`} className="flex gap-2 items-center"><ArrowLeftIcon className="size-5" />Назад к уроку</Link>
+            {showConfetti && <Fireworks autorun={{ speed: 2, duration: 5000 }} />}
+            <Link href={`/lessons/${lesson.id}`} className="flex gap-2 items-center"><ArrowLeftIcon className="size-5" />Назад
+                к уроку</Link>
             {/* Header */}
             <div className="flex justify-between md:items-center flex-col md:flex-row gap-3 md:gap-0">
                 <h1>Домашнее задание</h1>
-                {!lesson.isTasksCompleted && <Button onClick={complete} disabled={lesson.isTasksCompleted} variant="outline" className="md:text-sm">Завершить</Button>}
+                {!lesson.isTasksCompleted &&
+                    <Button onClick={complete} disabled={lesson.isTasksCompleted} variant="outline"
+                        className="md:text-sm">Завершить</Button>}
+                {lesson.isTasksCompleted && <p><b>Резльтат выполнения:</b> {lesson.result}</p>}
             </div>
             {/* Content */}
             <TasksView
--- components/SubscriptionsFeed/Item.tsx
+++
@@ -1,5 +0,0 @@
-import IUserSubscription from "@/types/user_subscription";
-import { formattedDate } from "@/utils/date";
-
-export default function SubscriptionsFeedItem({ subscription }: { subscription: IUserSubscription }) {
-}
