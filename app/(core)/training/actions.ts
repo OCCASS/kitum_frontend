23423d842b82648a4d5144794aab61674eb89fc1
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-23 16:15:02 +0300

    refactor: type errors refactoring

--- app/(core)/training/actions.ts
+++ app/(core)/training/actions.ts
@@ -2,11 +2,13 @@
 
 import {post} from "@/lib/fetch";
 import {redirect} from "next/navigation";
+import IVariant from "@/types/variant";
 
 export async function generateVariant(prevState: any, formData: FormData) {
-    const {data, status} = await post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/variants/generate/`, {
+    const {data, status} = await post<IVariant>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/variants/generate/`, {
         name: formData.get("name"),
         complexity: formData.get("complexity")
     })
     if (status === 200) redirect(`/variants/${data.id}/`)
+    return {}
 }
