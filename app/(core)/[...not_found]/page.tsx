4783f2e748bba9ec64281c15ea4f595d67581672
OCCCAS <pavlov.timur556@yandex.ru>
2024-07-25 00:55:28 +0300

    feat: created 404 page

---
+++ app/(core)/[...not_found]/page.tsx
@@ -0,0 +1,14 @@
+import Whale404Icon from "@/components/icons/Whale404Icon";
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <div className="flex justify-center items-center h-full">
+            <div>
+                <Whale404Icon className="m-auto w-full h-64 md:h-76 mb-5" />
+                <h1 className="text-center text-gray-text mb-2">Страница не найдена</h1>
+                <p className="text-center text-lg">Перейдите на <Link href="/" className="text-blue">главную</Link></p>
+            </div>
+        </div>
+    )
+}
