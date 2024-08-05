1b620b1a30d1fc52bd57b0cff6e808994a4d2328
OCCCAS <pavlov.timur556@yandex.ru>
2024-08-05 17:10:54 +0300

    feat: changed Button to LoadingButton in Subscription card

--- app/(core)/subscriptions/component.tsx
+++ app/(core)/subscriptions/component.tsx
@@ -1,23 +1,30 @@
 "use client"
 
 import ISubscription from "@/types/subscription";
-import Button from "@/components/ui/Button";
-import { post } from "@/lib/fetch";
+import {post} from "@/lib/fetch";
+import LoadingButton from "@/components/ui/LoadingButton";
+import {useState} from "react";
 
-export default function Subscription({ subscription }: { subscription: ISubscription }) {
+export default function Subscription({subscription}: { subscription: ISubscription }) {
+    const [isLoading, setIsLoading] = useState(false)
+
     const onClick = async () => {
-        const { data, status } = await post<{ confirmationUrl: string }>(
+        setIsLoading(true)
+        const {data, status} = await post<{ confirmationUrl: string }>(
             `${process.env.NEXT_PUBLIC_API_BASE_URL}/subscriptions/${subscription.id}/order/`,
-            { returnUrl: process.env.NEXT_PUBLIC_ROOT_URL, description: subscription.title }
+            {returnUrl: process.env.NEXT_PUBLIC_ROOT_URL, description: subscription.title}
         )
+        setIsLoading(false)
         if (status === 200) window.location.href = data.confirmationUrl
     }
 
     return (
-        <article className="card space-y-3" key={subscription.id}>
-            <h2>{subscription.title}</h2>
-            <p>Цена: <b>{subscription.price}/месяц</b></p>
-            <Button onClick={onClick}>Купить</Button>
+        <article className="card space-y-3 flex flex-col items-start" key={subscription.id}>
+            <div className="flex-1 space-y-3">
+                <h2>{subscription.title}</h2>
+                <p>Цена: <b>{subscription.price}/месяц</b></p>
+            </div>
+            <LoadingButton className="inline" onClick={onClick} isLoading={isLoading}>Купить</LoadingButton>
         </article>
     )
 }
