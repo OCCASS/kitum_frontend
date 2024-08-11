b11742f485eec5d6c4849fca5c0426a3aaedda1b
OCCCAS <pavlov.timur556@yandex.ru>
2024-08-11 20:09:53 +0300

    fix: when click to modal it closed fixed

--- components/ui/Modal.tsx
+++ components/ui/Modal.tsx
@@ -1,14 +1,23 @@
 import React, {Dispatch, SetStateAction, useEffect} from "react";
 import Button from "@/components/ui/Button";
 import {XMarkIcon} from "@heroicons/react/24/outline";
+import {twMerge} from "tailwind-merge";
 
 export default function Modal({
                                   title,
                                   show,
                                   setShow,
                                   closable = true,
+                                  className,
                                   children
-                              }: { title: string, show: boolean, setShow: Dispatch<SetStateAction<boolean>>, closable?: boolean, children: React.ReactNode }) {
+                              }: {
+    title: string,
+    show: boolean,
+    setShow: Dispatch<SetStateAction<boolean>>,
+    className?: string
+    closable?: boolean,
+    children: React.ReactNode
+}) {
     const close = () => {
         if (closable) setShow(false)
     }
@@ -19,16 +28,17 @@ export default function Modal({
     }, [show])
 
     return (
-        <div className={`${show ? "visible opacity-100" : "invisible opacity-0"} transition-all overlay overflow-auto`}
-             onClick={close}>
-            <div className="popup">
+        <div
+            className={twMerge("transition-all overlay overflow-auto", show ? "visible opacity-100" : "invisible opacity-0")}
+            onClick={close}>
+            <div className="popup" onClick={e => e.stopPropagation()}>
                 <div className="popup__header">
                     <div className="h-full flex justify-between items-center">
                         <h2>{title}</h2>
                         {closable && <Button variant="none" onClick={close}><XMarkIcon className="size-6"/></Button>}
                     </div>
                 </div>
-                <div className="overflow-y-scroll pb-5 px-6">
+                <div className={twMerge("overflow-y-scroll pb-5 px-6", className)}>
                     {children}
                 </div>
             </div>
