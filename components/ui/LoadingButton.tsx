278b4bc0c167e4f3d39852951d6f399c37c23244
OCCCAS <pavlov.timur556@yandex.ru>
2024-08-12 09:26:12 +0300

    refactor: decline click when loading or disabled

--- components/ui/LoadingButton.tsx
+++ components/ui/LoadingButton.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react"
+import React, {ButtonHTMLAttributes, FC, MouseEventHandler} from "react"
 import Spinner from "./Spinner"
 import Button from "@/components/ui/Button";
 import {twMerge} from "tailwind-merge";
@@ -10,9 +10,14 @@ export type TLoadingButtonProps =
     isLoading: boolean
 }
 
-const LoadingButton: FC<TLoadingButtonProps> = ({className, isLoading, children, disabled, variant, ...props}) => {
+const LoadingButton: FC<TLoadingButtonProps> = ({className, isLoading, children, disabled, variant, onClick, ...props}) => {
+    const onClickWrapper = (e: React.MouseEventHandler<HTMLButtonElement, MouseEvent>) => {
+        if (disabled || isLoading) return
+        if (onClick) onClick(e)
+    }
+
     return (
-        <Button className={twMerge("relative", className)} variant={variant} disabled={disabled} {...props}>
+        <Button className={twMerge("relative", className)} variant={variant} disabled={disabled} onClick={onClickWrapper} {...props}>
             <div className={isLoading ? "invisible" : "visible"}>{children}</div>
             {isLoading && <Spinner className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"/>}
         </Button>
