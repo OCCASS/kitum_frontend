6b2bca087771c3253626ca5a636a839652412aef
OCCCAS <pavlov.timur556@yandex.ru>
2024-08-19 01:37:28 +0300

    feat: added CORS

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,16 @@ import { type NextRequest, NextResponse } from "next/server"
 import { getTokenCookie, verifySession } from "@/lib/session"
 import { refresh } from "@/app/actions"
 
+function addCors(res: NextResponse): NextResponse {
+    res.headers.set("Access-Control-Allow-Credentials", "true")
+    return res
+}
+
 function signinResponse(req: NextRequest): NextResponse {
     const response = NextResponse.redirect(new URL(`/signin?redirect_to=${encodeURIComponent(process.env.NEXT_PUBLIC_ROOT_URL + req.nextUrl.pathname)}`, req.nextUrl))
     response.cookies.set("access", "", { maxAge: -1 })
     response.cookies.set("refresh", "", { maxAge: -1 })
-    return response
+    return addCors(response)
 }
 
 export default async function middleware(req: NextRequest) {
@@ -34,14 +39,14 @@ export default async function middleware(req: NextRequest) {
                 return signinResponse(req)
             }
         }
-        return NextResponse.next()
+        return addCors(NextResponse.next())
     } else if (onlyPublicRoutes.includes(currentPath)) {
         if (accessVerified && refreshVerified) {
-            return NextResponse.redirect(new URL(req.nextUrl.origin))
+            return addCors(NextResponse.redirect(new URL(req.nextUrl.origin)))
         }
     }
 
-    return NextResponse.next()
+    return addCors(NextResponse.next())
 }
 
 // Routes middleware should *not* run on
