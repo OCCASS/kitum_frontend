f9e78193ecc2dcaf1ec5bcf95b2ff82658924639
Тимур Павлов <pavlov.timur556@yandex.ru>
2025-05-31 20:23:11 +0300

    feat: added avatar placeholder

--- components/ui/UserProfileImage.tsx
+++ components/ui/UserProfileImage.tsx
@@ -1,14 +1,24 @@
 import IUser from "@/types/user";
 import Image from "next/image";
 
-export default function UserProfileImage({ user, src, size }: { user: IUser, src?: string, size?: number }) {
-    return <Image
-        src={src ? src : user.avatar}
-        alt={user.firstName.charAt(0) + user.lastName.charAt(0)}
-        width="128"
-        height="128"
-        className={"rounded-full object-cover"}
-        style={{ width: size, height: size }}
-        priority
-    />
+export default function UserProfileImage({ user, src, size = 128 }: { user: IUser, src?: string, size?: number }) {
+    return (
+        src || user.avatar ?
+            <Image
+                src={src ? src : user.avatar}
+                alt={`${user.firstName} ${user.lastName}`}
+                width={size}
+                height={size}
+                priority
+            />
+            :
+            <div style={{ width: size, height: size }} className={`bg-[#6c70f6] rounded-full relative`}>
+                <span
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#121481]"
+                    style={{ fontSize: size * 0.3 }}
+                >
+                    {user.firstName[0]} {user.lastName[0]}
+                </span>
+            </div>
+    )
 }
